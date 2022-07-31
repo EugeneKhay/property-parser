a9d7f82cfa7ac27be4a54e1e0d740a656da3a579
EugeneKhay <4358514@gmail.com>
2022-07-31 14:46:13 +0300

    added cli arguments flags

--- parser.py
+++ parser.py
@@ -7,26 +7,47 @@
 import os
 import sys
 import json
+import argparse
 from os.path import exists
 from jproperties import Properties
 
 ARGS_MINIMAL_NUMBER = 3
 PROPERTIES_EXTENSION = '.properties'
 JSON_EXTENSION = '.json'
-TARGET_DIR = sys.argv[2] 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--source", help="Source path")
+parser.add_argument("-t", "--target", help="Target path")
+parser.add_argument("-ct", "--createtarget", help="Should target be created if not exists", type=int)
+args = parser.parse_args()
+
+target_dir = args.target
+
+# param: 0 -False, 1 - True
+def is_creating_files_mode():
+    if args.createtarget != None:
+        return bool(args.createtarget)
+    return True
+    
     
 def parse():          
-    if len(sys.argv) < ARGS_MINIMAL_NUMBER:
-        print(f'Wrong parameters number: {len(sys.argv) - 1} is provided, 2 is needed')
-        print('Parameters are: 1 - source dir, 2 - target dir')
+    if not args.source:
+        print('No source provided')
         exit()
+    if not args.target:
+        print('No target provided')
+        exit()    
         
     try:
-        global TARGET_DIR
-        if (not exists(TARGET_DIR)):
-                print(f'Target directory parameter {TARGET_DIR} does not exist, creating it ...')
-                os.makedirs(TARGET_DIR) 
-        source = sys.argv[1]
+        global target_dir
+        if (not exists(target_dir)):
+            if is_creating_files_mode():
+                print(f'Target directory parameter {target_dir} does not exist, creating it ...')
+                os.makedirs(target_dir)
+            else:
+                print(f'Target directory parameter {target_dir} does not exist, exiting ...')
+                exit()  
+        source = args.source
         
         if os.path.isfile(source) and source.endswith(PROPERTIES_EXTENSION):
             filename = source.rsplit(os.sep, 1)[1]
@@ -45,7 +66,7 @@ def parse():
         
             
 def create_json_file_name(properties_file):
-    return TARGET_DIR + '/' + properties_file.rsplit('.', 1)[0] + JSON_EXTENSION        
+    return target_dir + '/' + properties_file.rsplit('.', 1)[0] + JSON_EXTENSION        
             
             
 def write_props_to_json(properties_file, full_path):
