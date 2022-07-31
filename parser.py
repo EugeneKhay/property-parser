1c4a8bb870cf6178b6369e76c9532262622d5411
EugeneKhay <4358514@gmail.com>
2022-07-31 13:14:47 +0300

    version 1.0

---
+++ parser.py
@@ -0,0 +1,70 @@
+# pip3 install jproperties
+
+# Script should be triggered with at least 3 parameters:
+# script name, source directory, target directory, i.e:
+# python3 parser.py ./source_dir ./target_dir
+
+import os
+import sys
+import json
+from os.path import exists
+from jproperties import Properties
+
+ARGS_MINIMAL_NUMBER = 3
+TARGET_DIR = sys.argv[2] 
+    
+def parse():          
+    if len(sys.argv) < ARGS_MINIMAL_NUMBER:
+        print(f'Wrong parameters number: {len(sys.argv) - 1} is provided, 2 is needed')
+        print('Parameters are: 1 - source dir, 2 - target dir')
+        exit()
+        
+    try:
+        global TARGET_DIR
+        if (not exists(TARGET_DIR)):
+                print(f'Target directory parameter {TARGET_DIR} does not exist, creating it ...')
+                os.makedirs(TARGET_DIR) 
+        source_dir = sys.argv[1]
+        
+        if os.path.isfile(source_dir) and source_dir.endswith('.properties'):
+            filename = source_dir.rsplit(os.sep, 1)[1]
+            print(filename)
+            print(source_dir)
+            write_props_to_json(filename, source_dir)
+        else:    
+            files_list = os.listdir(source_dir)
+            for file in files_list:
+                if (file.endswith(".properties")):
+                    path = f'{source_dir}/{file}'
+                    print(f'Processing file: {path} ...')
+                    write_props_to_json(file, path)        
+    except FileNotFoundError:
+        print(f'Source directory parameter {source_dir} is incorrect')
+        exit()
+        
+            
+def create_json_file_name(properties_file):
+    return TARGET_DIR + '/' + properties_file.rsplit('.', 1)[0] + '.json'        
+            
+            
+def write_props_to_json(properties_file, full_path):
+    configs = Properties()
+
+    with open(full_path, 'rb') as config_file:
+        configs.load(config_file)
+        
+    items = configs.items()
+    dict = {}
+
+    for item in items:
+        dict[item[0]] = item[1].data
+            
+    json_file = create_json_file_name(properties_file)  
+        
+    with open(json_file, "w") as outfile:
+        json.dump(dict, outfile, indent=2)
+        print(f'File {json_file} created')            
+            
+            
+if __name__ == '__main__':
+    parse()            
